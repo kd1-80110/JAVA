d908ecabc874750b8796a47fc944151b545bd565
suyash6643 <moresuyash939@gmail.com>
2023-10-18 19:50:49 +0530

    Assignment 10 uploaded successfully

---
+++ Assignment_10/Question3/src/com/sunbeam/Employee.java
@@ -0,0 +1,76 @@
+package com.sunbeam;
+
+import java.util.Scanner;
+
+public class Employee {
+	private int id;
+	private String name;
+	private double salary;
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public void accept() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the ID = ");
+		id = sc.nextInt();
+		System.out.println("Enter the Name = ");
+		name = sc.next();
+		System.out.println("Enter the Salary = ");
+		salary = sc.nextDouble();
+
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (obj instanceof Employee) {
+			Employee other = (Employee) obj;
+			this.name.equals(other.name);
+			return true;
+		}
+
+		return false;
+	}
+
+}
